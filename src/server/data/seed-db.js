bfbcb827d292d92063b61ec1ec39259fb109b316
Damien <dasseya1@gmail.com>
2018-01-25 23:00:50 -0500

    Set up connexion to mongodb

--- src/server/data/seed-db.js
+++ src/server/data/seed-db.js
@@ -10,5 +10,22 @@ const bcrypt = require('bcrypt');
 function seedCollection(collectionName, initialRecords) {
     MongoClient.connect(process.env.DB_CONN, (err, db) => {
         console.log('Connected to mongodb ...');
+
+        const collection = db.collection(collectionName);
+        collection.remove();
+        initialRecords.forEach(element => {
+            if (item.password) {
+                item.password = bcrypt.hashSync(item.password, 10);
+            }
+        });
+        collection.insertMany(initialRecords, (err, result) => {
+            console.log(`${result.insertedCount} records inserted.`);
+            console.log('closing connexion...');
+            db.close();
+            console.log('done');
+        })
     })
 }
+
+seedCollection('users', users);
+seedCollection('contacts', contacts);
